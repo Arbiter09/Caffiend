21c27bd02cfe4dae04a32fa0aebd2ae32efe0da1
Arbiter09 <100960442+Arbiter09@users.noreply.github.com>
2025-03-31 02:47:21 -0400

    Completed Static Web Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,10 +3,23 @@ export default function Layout(props) {
     const {children} = props
 
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1 className="text-gradient">CAFFIEND</h1>
+                <p>For Coffeee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign Up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
     const footer = (
-        <footer></footer>
+        <footer>
+            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://github.com/Arbiter09">Jas!</a>
+            <br /> <a target="_blank" href="https://www.linkedin.com/in/jas-shah-709854233/">LinkedIn</a>
+            </p>
+        </footer>
     )
 
     return (
